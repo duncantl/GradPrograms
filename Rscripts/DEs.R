e93a29d6bb3b4a07db9ac2ca8d5a2a77b37a0b78
Duncan Temple Lang <duncan@r-project.org>
2024-12-07 22:44:12 -0800

    make the name of the cache directory a variable

--- Rscripts/DEs.R
+++ Rscripts/DEs.R
@@ -10,9 +10,10 @@ ll = getNodeSet(doc, "//a[starts-with(@href, '/programs/designated-emphases/g')]
 h = structure(sapply(ll, xmlGetAttr, "href"), names = sapply(ll, xmlValue))
 de.urls = getRelativeURL(h, u)
 
-if(!file.exists("DECache"))
-    dir.create("DECache")
-filenames = sprintf("DECache/%s.html", basename(de.urls))
+deDir = "DECache"
+if(!file.exists(deDir))
+    dir.create(deDir)
+filenames = sprintf("%s/%s.html", deDir, basename(de.urls))
 names(filenames) = names(de.urls)
 mapply(download.file, de.urls, filenames)
 
