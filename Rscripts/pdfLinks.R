67dab2e235637462eae75d862790360d3d20d924
Duncan Temple Lang <duncan@r-project.org>
2024-12-07 22:44:47 -0800

    funcion to get the URLs

--- Rscripts/pdfLinks.R
+++ Rscripts/pdfLinks.R
@@ -7,6 +7,7 @@ library(Rqpdf)
 source("~/DSIProjects/Rqpdf/R/links.R")
 
 library(XML)
+library(ReadPDF)
 
 
 pdfs = list.files(dir, full = TRUE)
@@ -17,3 +18,13 @@ tt = dsort(table(unlist(lnks)))
 z = grepl("^https?:", names(tt))
 us = lapply(names(tt)[z], parseURI)
 h = sapply(us, `[[`, "server")
+
+
+getURLs =
+function(pdf)    
+{
+    doc = readPDFXML(pdf)
+    xpathSApply(doc, "//text[contains(., 'http:') or contains(., 'https:')]", xmlValue, trim = TRUE)
+}
+
+u2 = lapply(pdfs, getURLs)
