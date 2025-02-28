e8ae4af0ee070927145fda6509f4c4e62088cdc6
Duncan Temple Lang <duncan@r-project.org>
2025-02-27 18:32:02 -0800

    reworked this code to get the program codes and then to create the program URLs in a different function

--- R/programCodes.R
+++ R/programCodes.R
@@ -1,11 +1,21 @@
+GSProgURL = "https://grad.ucdavis.edu/programs"
+
 programCodes = 
-function(url = "https://grad.ucdavis.edu/programs")
+function(url = GSProgURL)
 {
     txt = readLines(url)
     doc = htmlParse(txt)
     u = getHTMLLinks(doc)
-    w = grepl("^/programs/[a-z]{4}$", u)
 
+    w = grepl("^/programs/[a-z]{4}$", u)
     u = u[w]
-    getRelativeURL(u, url)
+
+    codes = toupper(gsub("^/programs/", "", u))
+}
+
+programURLs = 
+function(codes = programCodes(url), url = GSProgURL)
+{
+    structure( getRelativeURL(paste0("/programs/", tolower(codes)), url),
+               names = names(codes))
 }
