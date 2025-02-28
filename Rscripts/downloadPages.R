c559c86d808a2e3738d19f3a949d1a9d10e23d79
Duncan Temple Lang <duncan@r-project.org>
2025-02-27 18:45:26 -0800

    converted this script into 2 functions. One was already in the R/ directory (with some small adaptations) as programCodes.  Left this as a function foo but should kill this file when we decide no need for that version. The other was for downloading the program pages. Added this to the R/ directory.

--- Rscripts/downloadPages.R
+++ Rscripts/downloadPages.R
@@ -1,37 +1,34 @@
 library(RCurl)
 library(XML)
-u = "https://grad.ucdavis.edu/programs"
-doc = htmlParse(getURLContent(u, followlocation = TRUE))
 
-ll = getHTMLLinks(doc)
 
-# program-tab--alphabetical
-#lnodes = getNodeSet(doc, "//a[starts-with(@href, '/programs/')]")
-#lnodes = getNodeSet(doc, "//div[contains(@class, 'program-tab--alphabetical')]//a[starts-with(@href, '/programs/')]")
-lnodes = getNodeSet(doc, "//a[starts-with(@href, '/programs/')]")
-#h = sapply(lnodes, xmlGetAttr, "href")
-#w = grepl("^/programs/[a-z]{4}$", ll)
-#lnodes = lnodes[w]
 
-progs = structure(sapply(lnodes, xmlGetAttr, "href"), names = sapply(lnodes, xmlValue))
+# We have this in ../R/programCodes.R
+programNames = 
+function(u = GSProgURL, dir)
+{    
+    doc = htmlParse(getURLContent(u, followlocation = TRUE))
+    ll = getHTMLLinks(doc)
 
-progs = progs[!duplicated(progs)]
+    # program-tab--alphabetical
+    #lnodes = getNodeSet(doc, "//a[starts-with(@href, '/programs/')]")
+    #lnodes = getNodeSet(doc, "//div[contains(@class, 'program-tab--alphabetical')]//a[starts-with(@href, '/programs/')]")
+    lnodes = getNodeSet(doc, "//a[starts-with(@href, '/programs/')]")
+    #h = sapply(lnodes, xmlGetAttr, "href")
+    #w = grepl("^/programs/[a-z]{4}$", ll)
+    #lnodes = lnodes[w]
 
-progs2 = structure(sapply(lnodes, xmlValue), names = gsub("/programs/", "", sapply(lnodes, xmlGetAttr, "href")))
+    progs = structure(sapply(lnodes, xmlGetAttr, "href"), names = sapply(lnodes, xmlValue))
+
+    progs = progs[!duplicated(progs)]
+
+    progs2 = structure(sapply(lnodes, xmlValue), names = gsub("/programs/", "", sapply(lnodes, xmlGetAttr, "href")))
+}
 
 # progs is path to program e.g. /programs/gven with the name being the Human readable program name Viticulture and Enology
 # progs2 is the mapping between human readable name and 4 letter program code, the latter being the names on the progs2 vector.
 
 
 #progs = grep("^/programs/[a-z]{4}$", ll, value = TRUE)
-u.progs = getRelativeURL(progs, u)
 
-
-# Download the pages locally for each program.
-dir = gsub("-", "_", sprintf("Cache_%s", Sys.Date()))
-if(!file.exists(dir))
-    dir.create(dir)
-filenames = file.path( dir, sprintf("%s.html", basename(u.progs)))
-names(filenames) = names(u.progs)
-mapply(download.file, u.progs, filenames)
 
