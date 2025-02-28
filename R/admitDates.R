b575bb317715637e841c85f2319c9ef50fbcdfdb
Duncan Temple Lang <duncan@r-project.org>
2025-02-27 18:41:57 -0800

    a script for computing all space available dates. Could turn into a function.  We also have this information in a spreadsheet so could just get it from there.

--- R/admitDates.R
+++ R/admitDates.R
@@ -1,24 +1,7 @@
-# For the old page format, before SiteFarm.
-# XXX Remove this file from git.
-# 
-appDates =
-function(f, doc = htmlParse(f))
-{
-    z = getNodeSet(doc, "//h3[. = 'Application Deadlines']/following-sibling::div")
-    if(length(z) == 0)
-        return(as.Date(character()))
-    
-    d = sapply(z, function(n) xpathSApply(n, ".//div[contains(@class, 'field__item even')]", xmlValue))
-    d = as.Date(d, "%b %d, %Y")
-    names(d) = gsub(":.*", "", sapply(z, function(n) xpathSApply(n, ".//div[contains(@class, 'field__label')]", xmlValue)))
-    
-    d
-}
-
-
+# Potentially turn into a function.
 if(FALSE) {
     ff = list.files("Cache5", full = TRUE)
-    appdates = lapply(ff, appDates)
+    appdates = lapply(ff, admissionDate)
     names(appdates) = gsub("\\.html$", "", basename(ff))
     sa = lapply(appdates, function(x) x["Space Available"])
     sa = sa[!sapply(sa, is.na)]
