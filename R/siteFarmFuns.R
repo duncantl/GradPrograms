6ebcbdbfe74b90aa33211d555050576441dd4aae
Duncan Temple Lang <duncan@r-project.org>
2025-02-27 18:34:46 -0800

    removed the leadDean function from here. See funs2.R

--- R/siteFarmFuns.R
+++ R/siteFarmFuns.R
@@ -1,10 +1,3 @@
-leadDean =
-function(f, doc = htmlParse(f))
-{
-  xpathSApply(doc, "//div[contains(@class , 'field--name-field-lead-dean')]/div[not(. = 'Lead Dean')]", xmlValue)
-}
-
-
 degReqDates =    
 function(f, doc = htmlParse(f))
 {
