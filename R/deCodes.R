37307a82573076691d9d7b5e0ab700d7a02a51af
Duncan Temple Lang <duncan@r-project.org>
2025-06-23 07:46:16 -0700

    function to get the GAC/GAUC codes and names

--- R/deCodes.R
+++ R/deCodes.R
@@ -11,3 +11,20 @@ function()
     structure(toupper(basename(h)), names = sapply(a, xmlValue))
 }
 
+
+gcertCodes = gaucCodes =
+    function()
+{
+    u =  "https://grad.ucdavis.edu/graduate-academic-certificates"
+    doc = htmlParse(readLines(u))
+    li = getNodeSet(doc, "//ul[@class = 'list--accordion']/li")
+    idx = seq(1, by = 2, length = length(li)/2)
+    ti = sapply(li[idx], xmlValue)
+    rx = "^([^(]+) \\((G[A-Z]+)\\)$"
+
+    nms = gsub(rx, "\\1", ti)
+    # nbsp
+    nms = gsub("\ua0", '', nms)
+
+    structure( gsub(rx, "\\2", ti), names = nms)
+}
